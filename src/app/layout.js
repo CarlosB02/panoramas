207d405c36920af70a8d4ba10b0ff9fbdd154d3b
CarlosB02 <carlosbernardo.2002@gmail.com>
2026-04-10 19:42:40 +0100

    Update layout.js
    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './index.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 export const metadata = {
     title: {
@@ -19,6 +20,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="pt" suppressHydrationWarning>
+            <head>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-V7MJMLJ3E6"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-V7MJMLJ3E6');
+                    `}
+                </Script>
+            </head>
             <body suppressHydrationWarning>
                 <div className="layout-wrapper">
                     <Header />
